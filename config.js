e0a423fd20458c662c249ca8a284c5d17a6adcba
GrimmOutlook <grimmoutlook87@gmail.com>
2018-02-20 16:46:07 -0500

    Changed file test.users.js to test-users.js.  Uncommented TEST_DATABASE_URL in config.js.  Correct test db now listed at top of test files.

--- config.js
+++ config.js
@@ -7,8 +7,6 @@ exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 
-
-// exports.TEST_DATABASE_URL = (
-//   process.env.TEST_DATABASE_URL ||
-//   'mongodb://localhost/test-whiskeytest');
-
+exports.TEST_DATABASE_URL = (
+  process.env.TEST_DATABASE_URL ||
+  'mongodb://localhost/test-tgp');
